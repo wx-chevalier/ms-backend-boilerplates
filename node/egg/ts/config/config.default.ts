14d0ef68ddd9da7d56c9fe91a5f2df16d6e9b566
wxyyxc1992 <384924552@qq.com>
2018-05-23 00:25:21 +0800

    feat: node/egg/ts

---
+++ node/egg/ts/config/config.default.ts
@@ -0,0 +1,25 @@
+import { EggAppConfig, PowerPartial } from 'egg';
+
+// for config.{env}.ts
+export type DefaultConfig = PowerPartial<EggAppConfig & BizConfig>;
+
+// app special config scheme
+export interface BizConfig {
+  sourceUrl: string;
+}
+
+export default (appInfo: EggAppConfig) => {
+  const config = {} as PowerPartial<EggAppConfig> & BizConfig;
+
+  // app special config
+  config.sourceUrl = `https://github.com/eggjs/examples/tree/master/${appInfo.name}`;
+
+  // override config from framework / plugin
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1525139663583_4770';
+
+  // add your config here
+  config.middleware = [];
+
+  return config;
+};
--- node/graphql/express/README.md
+++ node/graphql/express/README.md
@@ -4,6 +4,8 @@
 
 * 使用 [Sequelize Mock](https://sequelize-mock.readthedocs.io/en/stable/) 作为 Sequelize 的 Mock 框架，以模拟 ORM 操作而不受限于真实的数据库服务器连接，并且方便进行单元测试。
 
+* 使用 [db4free](https://www.db4free.net/signup.php) 创建了免费的账户。
+
 # Usage | 使用
 
 # Motivation & Credits
