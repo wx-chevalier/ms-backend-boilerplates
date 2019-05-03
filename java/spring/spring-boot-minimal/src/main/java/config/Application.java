3feed9b7c65e63feb3539cd35aa7a1c2b4fe38bf
wxyyxc1992 <384924552@qq.com>
2019-05-03 22:37:07 +0800

    chore: update spring-boot-minimal

---
+++ java/spring/spring-boot-minimal/src/main/java/config/Application.java
@@ -0,0 +1,40 @@
+package config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@Slf4j
+public class Application {
+
+    Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Spring World";
+    }
+
+
+
+    @RequestMapping("/log")
+    public String index() {
+        logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+
+        return "Howdy! Check out the Logs to see the output...";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
--- java/spring/spring-boot-minimal/src/test/java/config/HelloWorldConfigurationTests.java
+++ java/spring/spring-boot-minimal/src/test/java/config/HelloWorldConfigurationTests.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package hello;
+package config;
 
 import static org.junit.Assert.*;
 
