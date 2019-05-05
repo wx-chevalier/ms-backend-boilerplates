a3d001dc23d8f8cf9a5a5b290a8b0c727d8c2d3a
wxyyxc1992 <384924552@qq.com>
2019-05-05 15:10:05 +0800

    feat: add dynamic ds

--- java/spring/spring-security-jwt/src/main/java/wx/controller/AuthController.java
+++ java/spring/spring-security-jwt/src/main/java/wx/controller/AuthController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/token")
-public class AuthenticationController {
+public class AuthController {
 
   @Autowired private AuthenticationManager authenticationManager;
 
