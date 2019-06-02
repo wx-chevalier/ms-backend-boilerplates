e3f3285492e6feb30f9a461404fa0be8129619a4
wxyyxc1992 <384924552@qq.com>
2019-06-02 18:19:02 +0800

    chore: update

--- db/mysql/mysql-mall-matrix/docker-entrypoint-initdb.d/0-init.sql
+++ db/mysql/mysql-mall-matrix/docker-entrypoint-initdb.d/0-init.sql
@@ -1,6 +1,3 @@
-drop database if exists mall-matrix;
-create database mall-matrix
-default character
-set utf8mb4
-collate utf8mb4_unicode_ci;
-use mall-matrix;
+drop database if exists `mall-matrix`;
+create database `mall-matrix` default character set utf8mb4 collate utf8mb4_general_ci;
+use `mall-matrix`;
